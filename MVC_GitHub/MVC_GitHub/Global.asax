8980268712299ab5fe3826d5e67407fab90e181f
ialshayeb <ialshayeb@TRANLAB001>
2016-11-16 17:22:39 +0200

    ibrahim

---
+++ MVC_GitHub/MVC_GitHub/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVC_GitHub.MvcApplication" Language="C#" %>
---
+++ MVC_GitHub/MVC_GitHub/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC_GitHub.Startup))]
+namespace MVC_GitHub
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
